c9951ba488c03bf64627eca3eef4dd22ee6cc6b7
BruceChen <bruce.mvc@outlook.com>
2015-12-21 00:08:29 +0800

    Init

---
+++ KKLib.Tests/TableStorageTests.cs
@@ -0,0 +1,158 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace KKLib.Tests
+{
+    [TestFixture()]
+    public class TableCacheTests
+    {
+        private string tableConn;
+        private string tableName;
+        private string partitionKey;
+
+        [SetUp()]
+        public void Init()
+        {
+            //!++執行測試前，請先啟動本機儲存體模擬器
+            this.tableConn = "UseDevelopmentStorage=true";
+            this.tableName = "itest";
+            this.partitionKey = "testPK";
+        }
+
+        [TearDown()]
+        public void Cleanup()
+        {
+        }
+
+        [Test()]
+        public void Insert_插入實體至table_應取得OK回應()
+        {
+            var data = new TestEntity(partitionKey, "InsertTest") { Value = "InsertTest Data 1." };
+            var expected = "OK";
+
+            var table = new TableStorage<TestEntity>(tableConn, tableName);
+            var actual = table.Insert(data);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test()]
+        public void InsertOrReplace_先插入實體並進行實體覆寫_應取得OK回應()
+        {
+            var expected = "OK";
+
+            // Insert Test
+            var data = new TestEntity(partitionKey, "InsertTest2") { Value = "InsertTest2 Data 1." };
+            var table = new TableStorage<TestEntity>(tableConn, tableName);
+            var actual1 = table.InsertOrReplace(data);
+            Assert.AreEqual(expected, actual1);
+
+            // Replace Text
+            var data2 = new TestEntity(partitionKey, "InsertTest2") { Value = "[Replace]InsertTest2 Data 1." };
+            var actual2 = table.InsertOrReplace(data2);
+
+            Assert.AreEqual(expected, actual2);
+        }
+
+        [Test()]
+        public void Insert_進行Batch插入_應取得OK回應()
+        {
+            var data1 = new TestEntity(partitionKey, "batch1") { Value = "Batch Test Data 1." };
+            var data2 = new TestEntity(partitionKey, "batch2") { Value = "Batch Test Data 2." };
+            var data3 = new TestEntity(partitionKey, "batch3") { Value = "Batch Test Data 3." };
+            var datas = new List<TestEntity>();
+            datas.Add(data1);
+            datas.Add(data2);
+            datas.Add(data3);
+            var expected = "OK";
+
+            var table = new TableStorage<TestEntity>(tableConn, tableName);
+            var actual = table.BatchInsert(datas);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test()]
+        public void GetAll_取回所有實體集合()
+        {
+            var table = new TableStorage<TestEntity>(tableConn, tableName);
+            var datas = table.GetAll();
+
+            CollectionAssert.AllItemsAreInstancesOfType(datas, typeof(TestEntity));
+        }
+
+        [Test()]
+        public void GetAllAsync_非同步取回所有實體集合()
+        {
+            var table = new TableStorage<TestEntity>(tableConn, tableName);
+            var datas = table.GetAllAsync();
+
+            CollectionAssert.AllItemsAreInstancesOfType(datas.Result, typeof(TestEntity));
+        }
+
+        [Test()]
+        public void GetRange_取回某一Rowkey區間實體集合()
+        {
+
+            var table = new TableStorage<TestEntity>(tableConn, tableName);
+            var datas = table.GetRange("a");
+
+            CollectionAssert.AllItemsAreInstancesOfType(datas, typeof(TestEntity));
+        }
+
+        [Test()]
+        public void GetSingle_取回單一實體()
+        {
+            var data = new TestEntity(partitionKey, "batch1");
+
+            var table = new TableStorage<TestEntity>(tableConn, tableName);
+            var actual = table.GetSingle(data);
+
+            Assert.IsNotNull(actual);
+            StringAssert.Contains("Test", actual.Value);
+        }
+
+        [Test()]
+        public void Delete_刪除實體()
+        {
+            var data = new TestEntity(partitionKey, "batch1");
+            var expected = "OK";
+
+            var table = new TableStorage<TestEntity>(tableConn, tableName);
+            var actual = table.Delete(data);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test()]
+        public void DeleteTable_刪除表格()
+        {
+            var expected = "OK";
+
+            var table = new TableStorage<TestEntity>(tableConn, tableName);
+            var actual = table.DeleteTable();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    /// <summary>
+    /// 類別需繼承 TableEntity，並指定 PartitionKey 與 RowKey
+    /// </summary>
+    class TestEntity : TableEntity
+    {
+
+        public TestEntity(string PartitionKey, string RowKey)
+        {
+            this.PartitionKey = PartitionKey;
+            this.RowKey = RowKey;
+        }
+
+        // 規定
+        public TestEntity() { }
+
+        public string Value { get; set; }
+
+    }
+}
---
+++ KKLib/TableStorage.cs
@@ -0,0 +1,252 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace KKLib
+{
+    /// <summary>
+    /// Table Storage
+    /// </summary>
+    public class TableStorage<T> where T : TableEntity, new()
+    {
+        private string tableConn;
+        private string tableName;
+        private CloudTable table;
+
+        /// <summary>
+        /// 初始化 Table 物件。
+        /// </summary>
+        /// <param name="tableConn">連線字串</param>
+        /// <param name="tableName">存取表格名稱</param>
+        public TableStorage(string tableConn, string tableName)
+        {
+            this.tableConn = tableConn;
+            this.tableName = tableName;
+
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(tableConn);
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+            table = tableClient.GetTableReference(tableName);
+            table.CreateIfNotExists();
+        }
+
+        /// <summary>
+        /// 新增資料至Table儲存區。
+        /// 注意，不能存放重覆key值資料。
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public string Insert(T entity)
+        {
+            TableOperation insertOperation = TableOperation.Insert(entity);
+            try
+            {
+                table.Execute(insertOperation);
+            }
+            catch (Exception ex)
+            {
+                return $"Error:{ex.Message}";
+            }
+
+            return "OK";
+        }
+
+        /// <summary>
+        /// 新增或覆寫資料至Table儲存區。
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public string InsertOrReplace(T entity)
+        {
+            try
+            {
+                TableResult retrievedResult = GetServerTableResult(entity);
+                T updateEntity = (T)retrievedResult.Result;
+
+                if (updateEntity != null)
+                {
+                    // Replace
+                    updateEntity = entity;
+                    TableOperation insertOrReplaceOperation = TableOperation.InsertOrReplace(updateEntity);
+                    try
+                    {
+                        table.Execute(insertOrReplaceOperation);
+                        return "OK";
+                    }
+                    catch (Exception ex)
+                    {
+                        return $"Error:{ex.Message}";
+                    }
+                }
+                else
+                {
+                    // Insert
+                    TableOperation insertOrReplaceOperation = TableOperation.InsertOrReplace(entity);
+                    try
+                    {
+                        table.Execute(insertOrReplaceOperation);
+                        return "OK";
+                    }
+                    catch (Exception ex)
+                    {
+                        return $"Error:{ex.Message}";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return $"Error:{ex.Message}";
+            }
+        }
+
+        /// <summary>
+        /// 批次新增資料至Table儲存區。
+        /// </summary>
+        /// <param name="entities">欲快取的集合</param>
+        /// <returns></returns>
+        public string BatchInsert(IEnumerable<T> entities)
+        {
+            TableBatchOperation batchOperation = new TableBatchOperation();
+            foreach (var cache in entities)
+            {
+                batchOperation.Insert(cache);
+            }
+
+            try
+            {
+                table.ExecuteBatch(batchOperation);
+            }
+            catch (Exception ex)
+            {
+                return $"Error:{ex.Message}";
+            }
+
+            return "OK";
+        }
+
+        /// <summary>
+        /// 取得 table 所有集合。
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<T> GetAll()
+        {
+            TableQuery<T> query = new TableQuery<T>().Where(
+                TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, tableName));
+            return table.ExecuteQuery(query);
+        }
+
+        /// <summary>
+        /// 以非同步取得大量(1000個以上)實體。
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            TableQuery<T> tableQuery = new TableQuery<T>();
+            TableContinuationToken continuationToken = null;
+            do
+            {
+                // 接收一個段落資料(1000以上實體)
+                TableQuerySegment<T> tableQueryResult =
+                    await table.ExecuteQuerySegmentedAsync(tableQuery, continuationToken);
+                continuationToken = tableQueryResult.ContinuationToken;
+                return tableQueryResult.Results;
+            } while (continuationToken != null);
+        }
+
+        /// <summary>
+        /// 以 rowkey 為關鍵字，取得相關集合。
+        /// </summary>
+        /// <param name="rowkey"></param>
+        /// <returns></returns>
+        public IEnumerable<T> GetRange(string rowkey)
+        {
+            TableQuery<T> rangeQuery = new TableQuery<T>().Where(
+                TableQuery.CombineFilters(
+                    TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, tableName),
+                    TableOperators.And,
+                    TableQuery.GenerateFilterCondition("RowKey", QueryComparisons.LessThan, rowkey)));
+
+            return table.ExecuteQuery(rangeQuery);
+        }
+
+        /// <summary>
+        /// 取得單一實體
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public T GetSingle(T entity)
+        {
+            TableResult retrievedResult = GetServerTableResult(entity);
+            T singleData = (T)retrievedResult.Result;
+            if (singleData != null)
+            {
+                return singleData;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// 刪除實體
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public string Delete(T entity)
+        {
+            TableResult retrievedResult = GetServerTableResult(entity);
+            T deleteEntity = (T)retrievedResult.Result;
+            if (deleteEntity != null)
+            {
+                TableOperation deleteOperation = TableOperation.Delete(deleteEntity);
+                table.Execute(deleteOperation);
+                return "OK";
+            }
+            else
+            {
+                return "Error: deleteEntity is null.";
+            }
+        }
+
+        /// <summary>
+        /// 刪除資料表。
+        /// 在刪除後一段時間內，將無法重新建立同名表格。
+        /// </summary>
+        /// <returns></returns>
+        public string DeleteTable()
+        {
+            try
+            {
+                table.DeleteIfExists();
+                return "OK";
+            }
+            catch (Exception ex)
+            {
+                return $"Error:{ex.Message}";
+            }
+
+        }
+
+        /// <summary>
+        /// 取回伺服器上的Table結果集
+        /// </summary>
+        /// <param name="tableData"></param>
+        /// <returns></returns>
+        private TableResult GetServerTableResult(T tableData)
+        {
+            TableOperation retrieveOperation = TableOperation.Retrieve<T>(tableName, tableData.RowKey.ToString());
+            try
+            {
+                TableResult retrievedResult = table.Execute(retrieveOperation);
+                return retrievedResult;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+
+        }
+    }
+}
